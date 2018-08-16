9ea55e6c1e7c0930c7eca777fa45bb79d5ce0ed2
Colin Newell <colin.newell@gmail.com>
2018-08-16 17:45:49 +0100

    Fix a problem with using the Google ChromeDriver not getting sessionId
    Probably fixed in the ugliest way possible.

--- lib/WebDriver/Tiny.pm
+++ lib/WebDriver/Tiny.pm
@@ -277,14 +277,18 @@ sub window_switch($self, $handle) {
     $self;
 }
 
-sub _req($self, $method, $path, $args = undef) {
+sub _req($self, $method, $path, $args = {}) {
+    my $need_sessionId = delete $args->{_need_sessionId};
+
     my $reply = $self->[0]->request(
         $method,
         $self->[1] . $path,
-        { content => JSON::PP::encode_json( $args // {} ) },
+        { content => JSON::PP::encode_json( $args ) },
     );
 
-    my $value = eval { JSON::PP::decode_json( $reply->{content} )->{value} };
+    my $full_value = eval { JSON::PP::decode_json( $reply->{content} ) };
+    my $value = $full_value->{value};
+    $value->{sessionId} //= $full_value->{sessionId} if $need_sessionId;
 
     unless ( $reply->{success} ) {
         my $error = $value
